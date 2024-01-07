555ef01d0744ecb85a4e427268f11a7f9cd55264
Ellen <38250543+ellen364@users.noreply.github.com>
2024-01-07 19:29:10 +0000

    Add tmux configuration
    Adds a new tmux.conf file and adds the vim-tmux-navigator plugin to
    lunarvim to easily switch between tmux panes even when lunarvim is
    running in a pane. (Otherwise lunarvim tends to take over all the
    keybindings.)

--- config.lua
+++ config.lua
@@ -164,6 +164,23 @@ lvim.plugins = {
       }
     end,
   },
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<c-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  }
 }
 
 -- -- Autocommands (`:help autocmd`) <https://neovim.io/doc/user/autocmd.html>
